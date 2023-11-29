6f8005c9b41bd938214af4c9f01a955ed05e32bf
Roman lesyo <ladiginscormag@gmail.com>
2023-11-29 10:25:58 +0200

    feat: create app

---
+++ site-bratusy-new/src/pages/Main/index.jsx
@@ -0,0 +1,13 @@
+import Hero from '../../components/Hero/Hero'
+
+import s from './Main.module.css'
+
+function Main() {
+	return (
+		<section className={s.container}>
+			<Hero />
+		</section>
+	)
+}
+
+export default Main
